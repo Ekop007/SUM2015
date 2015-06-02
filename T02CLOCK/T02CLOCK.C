2cafb4253b9d7ecf29bc656e6b92fe9ed3af15ba
Kachsheev Sasha <alexkachsheev@mail.ru>
2015-06-02 18:41:00 +0400

    First release.

---
+++ T02CLOCK/T02CLOCK.C
@@ -0,0 +1,242 @@
+/* T01FWIN.C
+ * Пример базового макета программы под WinAPI.
+ * Создание и поддержка простейшего окна.
+ */
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <windows.h>
+
+/* Имя класса окна */
+#define WND_CLASS_NAME "My window class"
+ /* Пи */
+DOUBLE Pi = 3.14159265358979323846; 
+/* Ссылка вперед */
+LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
+                               WPARAM wParam, LPARAM lParam );
+
+/* Главная функция программы.
+ *   - дескриптор экземпляра приложения:
+ *       HINSTANCE hInstance;
+ *   - дескриптор предыдущего экземпляра приложения
+ *     (не используется и должно быть NULL):
+ *       HINSTANCE hPrevInstance;
+ *   - командная строка:
+ *       CHAR *CmdLine;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (INT) код возврата в операционную систему.
+ *   0 - при успехе.
+ */
+
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT ShowCmd )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+  /* HINSTANCE hIns = LoadLibrary("shell32"); */
+
+  /* Регистрация класса окна */
+  wc.style = CS_VREDRAW | CS_HREDRAW; /* Стиль окна: полностью перерисовывать
+                                       * при изменении вертикального или
+                                       * горизонтального размеров
+                                       * еще можно CS_DBLCLKS для добавления
+                                       * отработки двойного нажатия */
+  wc.cbClsExtra = 0; /* Дополнительное количество байт для класса */
+  wc.cbWndExtra = 0; /* Дополнительное количество байт для окна */
+  wc.hbrBackground = CreateSolidBrush(RGB(255, 255, 0));
+  wc.hCursor = LoadCursor(NULL, IDC_HAND); /* Загрузка курсора (системного) */
+  wc.hIcon = LoadIcon(NULL, IDI_ASTERISK); /* Загрузка пиктограммы (системной) */
+  wc.hInstance = hInstance; /* Дескриптор приложения, регистрирующего класс */
+  wc.lpszMenuName = NULL; /* Имя ресурса меню */
+  wc.lpfnWndProc = MyWindowFunc; /* Указатель на функцию обработки */
+  wc.lpszClassName = WND_CLASS_NAME;
+
+  /* Регистрация класса в системе */
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "ERROR", MB_OK);
+    return 0;
+  }
+
+  /* Создание окна */
+  hWnd =
+    CreateWindow(WND_CLASS_NAME,    /* Имя класса окна */
+      "Title",                      /* Заголовок окна */
+      WS_OVERLAPPEDWINDOW,          /* Стили окна - окно общего вида */
+      CW_USEDEFAULT, CW_USEDEFAULT, /* Позиция окна (x, y) - по умолчанию */
+      CW_USEDEFAULT, CW_USEDEFAULT, /* Размеры окна (w, h) - по умолчанию */
+      NULL,                         /* Дескриптор родительского окна */
+      NULL,                         /* Дескриптор загруженного меню */
+      hInstance,                    /* Дескриптор приложения */
+      NULL);                        /* Указатель на дополнительные параметры */
+
+  ShowWindow(hWnd, ShowCmd);
+  UpdateWindow(hWnd);
+
+  /* Запуск цикла сообщений окна */
+  while (GetMessage(&msg, NULL, 0, 0))
+    /* Передача сообщений в функцию окна */
+    DispatchMessage(&msg);
+
+  return 30;
+} /* End of 'WinMain' function */
+
+/* Функция обработки сообщения окна.
+ * АРГУМЕНТЫ:
+ *   - дескриптор окна:
+ *       HWND hWnd;
+ *   - номер сообщения (см. WM_***):
+ *       UINT Msg;
+ *   - параметр сообшения ('word parameter'):
+ *       WPARAM wParam;
+ *   - параметр сообшения ('long parameter'):
+ *       LPARAM lParam;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (LRESULT) - в зависимости от сообщения.
+ */
+VOID DrawArrow( INT h, INT w, INT bml, HDC hMemDC, DOUBLE angle ) /* Рисование стрелки */
+{
+  INT x, y;
+  x = bml * sin(angle * Pi / 180);
+  y = bml * cos(angle * Pi / 180);
+  SelectObject(hMemDC, GetStockObject(DC_PEN));
+  SetDCPenColor(hMemDC, RGB(255, 0, 0));
+  MoveToEx(hMemDC, w, h, NULL);
+  LineTo(hMemDC, w + x, h - y);
+}
+
+LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
+                               WPARAM wParam, LPARAM lParam )
+{
+   HDC hDC;
+  INT x, y;
+  CREATESTRUCT *cs;
+//  POINT pt;
+  SYSTEMTIME st;
+  CHAR Buf[100];
+  HFONT hFnt;
+  static BITMAP bm;
+  static HBITMAP hBm, hBmLogo;
+  static HDC hMemDC, hMemDCLogo;
+  static INT w, h;
+  switch (Msg)
+  {
+  case WM_CREATE:
+    cs = (CREATESTRUCT *)lParam;
+    SetTimer(hWnd, 111, 50, NULL);
+    hBmLogo = LoadImage(NULL, "Clock1.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    GetObject(hBmLogo, sizeof(bm), &bm);
+
+    /* создаем контекст в памяти */
+    hDC = GetDC(hWnd);
+    hMemDC = CreateCompatibleDC(hDC);
+    hMemDCLogo = CreateCompatibleDC(hDC);
+    ReleaseDC(hWnd, hDC);
+
+    SelectObject(hMemDCLogo, hBmLogo);
+    return 0;
+
+  case WM_SIZE:
+    w = LOWORD(lParam);
+    h = HIWORD(lParam);
+
+    /* создаем картинку */
+    if (hBm != NULL)
+      DeleteObject(hBm);
+
+    hDC = GetDC(hWnd);
+    hBm = CreateCompatibleBitmap(hDC, w, h);
+    ReleaseDC(hWnd, hDC);
+
+    SelectObject(hMemDC, hBm);
+    SendMessage(hWnd, WM_TIMER, 111, 0);
+    return 0;
+
+  case WM_TIMER:
+    /* Clear Background */
+    SelectObject(hMemDC, GetStockObject(NULL_PEN));
+    SelectObject(hMemDC, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(hMemDC, RGB(255, 255, 255));
+    Rectangle(hMemDC, 0, 0, w + 1, h + 1);
+
+    StretchBlt(hMemDC, w / 2 - bm.bmWidth / 2, h / 2 - bm.bmHeight / 2, bm.bmWidth, bm.bmHeight,
+      hMemDCLogo, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
+    GetLocalTime(&st);
+
+    DrawArrow(h / 2, w / 2, bm.bmWidth / 2, hMemDC, st.wSecond * 6);
+    DrawArrow(h / 2, w / 2, bm.bmWidth / 2.5, hMemDC, st.wMinute * 6);
+    DrawArrow(h / 2, w / 2, bm.bmWidth / 4, hMemDC, (st.wHour % 12) * 6);
+
+    hFnt = CreateFont(30, 0, 0, 0, FW_BOLD, TRUE, FALSE,
+      FALSE, RUSSIAN_CHARSET, OUT_DEFAULT_PRECIS,
+      CLIP_DEFAULT_PRECIS, PROOF_QUALITY,
+      VARIABLE_PITCH | FF_SWISS, "");
+    SelectObject(hMemDC, hFnt);
+    GetLocalTime(&st);
+    SetTextColor(hMemDC, RGB(0, 255, 0));
+    SetBkColor(hMemDC, RGB(255, 255, 0));
+    SetBkMode(hMemDC, TRANSPARENT);
+    
+    TextOut(hMemDC, 5 * w / 12, 7 * h / 9, Buf,
+      sprintf(Buf, "%02d:%02d:%02d (%02d.%02d.%d)",
+        st.wHour, st.wMinute, st.wSecond,
+        st.wDay, st.wMonth, st.wYear));
+
+    DeleteObject(hFnt);
+
+    SelectObject(hMemDC, GetStockObject(DC_PEN));
+    SelectObject(hMemDC, GetStockObject(DC_BRUSH));
+    InvalidateRect(hWnd, NULL, TRUE);
+    return 0;
+
+  case WM_CLOSE:
+    if (MessageBox(hWnd, "Are you shure to exit from program?",
+          "Exit", MB_YESNO | MB_ICONQUESTION) == IDNO)
+      return 0;
+    break;
+
+  case WM_LBUTTONDOWN:
+    SetCapture(hWnd);
+    x = w / 8 * sin(clock());
+    y = h / 8 * cos(clock());
+    SelectObject(hMemDC, GetStockObject(DC_PEN));
+    SetDCPenColor(hMemDC, RGB(255, 0, 0));
+    MoveToEx(hMemDC, w / 2, h / 2, NULL);
+    LineTo(hMemDC, w / 2, y);
+    return 0;
+
+  case WM_LBUTTONUP:
+    ReleaseCapture();
+    return 0;
+
+ /* case WM_MOUSEMOVE:
+    x = (SHORT)LOWORD(lParam);
+    y = (SHORT)HIWORD(lParam);
+    if (wParam & MK_LBUTTON)
+    {
+      Ellipse(hMemDC, x - 5, y - 5, x + 5, y + 5);
+    }
+    SelectObject(hMemDC, GetStockObject(DC_PEN));
+    SetDCPenColor(hMemDC, RGB(255, 0, 0));
+    MoveToEx(hMemDC, w / 2, h / 2, NULL);
+    LineTo(hMemDC, x, y);
+    return 0;  */
+
+  case WM_ERASEBKGND:
+    BitBlt((HDC)wParam, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
+    return 0;
+
+  case WM_DESTROY:
+    DeleteDC(hMemDC);
+    DeleteObject(hBm);
+    DeleteDC(hMemDCLogo);
+    DeleteObject(hBmLogo);
+    KillTimer(hWnd, 111);
+    PostQuitMessage(0);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+} /* End of 'MyWindowFunc' function */
+
+/* END OF 'T02DBLB.C' FILE */
